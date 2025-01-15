b83a11f65188bf03fc4f6666a693abd24b4e452f
AmexTevz <amextevz@yahoo.com>
2025-01-15 16:51:25 -0500

    slightly changed config_reader.py

--- src/utils/config_reader.py
+++ src/utils/config_reader.py
@@ -1,12 +1,20 @@
+import os
+
+
 def read_store_data(file_path):
+    absolute_path = file_path
     try:
-        with open(file_path, 'r') as file:
+        project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        absolute_path = os.path.join(project_root, file_path)
+
+        with open(absolute_path, 'r') as file:
             stores = [line.strip() for line in file if line.strip()]
             return stores
     except FileNotFoundError:
-        raise FileNotFoundError(f"Store data file not found: {file_path}")
+        raise FileNotFoundError(f"Store data file not found. \nTried paths:\n- {file_path}\n- {absolute_path}")
     except Exception as e:
-        raise Exception(f"Error reading store data: {str(e)}")
+        raise Exception(f"Error reading store data from {absolute_path}: {str(e)}")
+
 
 def get_all_stores():
     stores = read_store_data('src/data/stores.csv')
